eab9abf024553e906cd3d6937c1d84269d6394e6
caneroj1 <caneroj1@tcnj.edu>
2016-06-30 20:26:50 -0400

    refactored Board.

--- src/Game/Board.hs
+++ src/Game/Board.hs
@@ -17,38 +17,43 @@ import Data.Text (Text, pack)
 import Data.List (intercalate, transpose)
 import Data.List.Split (chunksOf)
 import Game.Piece
+import Data.Array
 
-type Row      = [Piece]
-newtype Board = Board { getBoard :: [Row] }
+-- An array to represent the board
+type BoardArray = Array (Int, Int) Piece
+
+newtype Board = Board { getBoard :: BoardArray }
+
+data Loc = Rows | Cols
 
 maxColumnIndex = totalColumns - 1
+maxRowIndex    = totalRows - 1
 totalColumns   = 3
+totalRows      = totalColumns
 
 emptyBoard :: Board
-emptyBoard = Board $ map makeRow [0..maxColumnIndex]
-  where makeRow row = [Empty 0 row, Empty 1 row, Empty 2 row]
+emptyBoard = Board $ array ((0, 0), (maxColumnIndex, maxRowIndex))
+                    [((x, y), Empty x y) | y <- [0..maxRowIndex],
+                                           x <- [0..maxColumnIndex]]
 
 drawBoard :: Board -> Text
-drawBoard Board { getBoard = [r1, r2, r3] } = pack $
-  drawRow r1 ++ buffer ++ drawRow r2 ++ buffer ++ drawRow r3
-  where drawRow = intercalate "|" . map show
-        buffer  = "\n-+-+-\n"
+drawBoard Board { getBoard = b } = pack $
+  drawRow 0 ++ buffer ++ drawRow 1 ++ buffer ++ drawRow 2
+  where drawRow r = intercalate "|" $
+                    map show [b ! (i, r) | i <- [0..maxColumnIndex]]
+        buffer    = "\n-+-+-\n"
 
 addPiece :: Piece -> Board -> Board
-addPiece p@(Empty _ _) Board{getBoard = b} = undefined
-addPiece piece Board{getBoard = b} = makeNewBoard
-  where makeNewBoard  = Board $ modify (concat b) (toIndex (x piece) (y piece))
-        toIndex i j   = totalColumns * j + i
-        modify ls idx = chunksOf totalColumns $ prefix ++ [piece] ++ suffix
-          where prefix = take idx       ls
-                suffix = drop (idx + 1) ls
+addPiece p@(Empty _ _) _           = undefined -- FIXME
+addPiece piece Board{getBoard = b} = Board makeNewBoard
+  where makeNewBoard = b // [((x piece, y piece), piece)]
         x (X xp _) = xp
         x (O xp _) = xp
         y (X _ yp) = yp
         y (O _ yp) = yp
 
 moves :: Board -> [Piece]
-moves Board{getBoard = b} = concatMap keepEmpties b
+moves Board{getBoard = b} = keepEmpties $ elems b
   where
     keepEmpties =
       filter (\case
@@ -57,8 +62,8 @@ moves Board{getBoard = b} = concatMap keepEmpties b
 
 victory :: Board -> (Piece -> Bool) -> Bool
 victory Board{getBoard = b} pfn =
-  checkRows b pfn ||
-  checkRows (transpose b) pfn ||
+  checkRowsOrColumns b Rows pfn ||
+  checkRowsOrColumns b Cols pfn ||
   checkDiagonals b pfn
 
 allXs :: Piece -> Bool
@@ -69,18 +74,18 @@ allOs :: Piece -> Bool
 allOs (O _ _) = True
 allOs _       = False
 
-checkRows :: [Row] -> (Piece -> Bool) -> Bool
-checkRows []     _   = False
-checkRows (r:rs) pfn = all pfn r || checkRows rs pfn
-
-checkDiagonals :: [Row] -> (Piece -> Bool) -> Bool
-checkDiagonals rs pfn = checkLeft rs || checkRight rs
+checkRowsOrColumns :: BoardArray -> Loc -> (Piece -> Bool) -> Bool
+checkRowsOrColumns barr loc pfn = go loc 0
   where
+    go loc pos
+      | pos == totalRows                                    = False
+      | all pfn $ pieces pos loc                            = True
+      | otherwise                                           = go loc (pos+1)
+    pieces pos Rows = [barr ! (i, pos) | i <- [0..maxColumnIndex]]
+    pieces pos Cols = [barr ! (pos, i) | i <- [0..maxRowIndex]]
 
-    checkLeft [[l, _, _],
-               [_, m, _],
-               [_, _, r]]  = pfn l && pfn m && pfn r
-
-    checkRight [[_, _, l],
-                [_, m, _],
-                [r, _, _]] = pfn l && pfn m && pfn r
+checkDiagonals :: BoardArray -> (Piece -> Bool) -> Bool
+checkDiagonals barr pfn = checkLeft || checkRight
+  where
+    checkLeft  = all pfn [barr ! (i, i)               | i <- [0..maxRowIndex]]
+    checkRight = all pfn [barr ! (i, maxRowIndex - i) | i <- [0..maxRowIndex]]
